74fc05e85ff689ff76bdc383a747e3ed690c9b71
hadi33 <zhqx_33@163.com>
2017-09-29 17:33:44 +0800

    nested view

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -60,18 +60,18 @@ sap.ui.define([
 		// 	this.getView().setModel(i18nModel,"i18n");
 		// },
 
-		onShowHello: function() {
-			//show a native javaScript alert
-			// alert("hello world");
-			// alert("hello world");
-			// MessageToast.show("hello world");
-			// read msg from i18n model
-			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			var sMsg = oBundle.getText("helloMsg",[sRecipient]);
-			// show message
-			MessageToast.show(sMsg);
-		}
+		// onShowHello: function() {
+		// 	//show a native javaScript alert
+		// 	// alert("hello world");
+		// 	// alert("hello world");
+		// 	// MessageToast.show("hello world");
+		// 	// read msg from i18n model
+		// 	var oBundle = this.getView().getModel("i18n").getResourceBundle();
+		// 	var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+		// 	var sMsg = oBundle.getText("helloMsg",[sRecipient]);
+		// 	// show message
+		// 	MessageToast.show(sMsg);
+		// }
 
 	});
 
