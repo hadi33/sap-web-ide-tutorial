1532484042c0b260d88fbf8f8635e051c0d0ca79
hadi33 <zhqx_33@163.com>
2017-10-02 22:12:50 +0800

    step 33

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -31,9 +31,12 @@ sap.ui.define([
 				oBinding.filter(aFilter);
 				
 			},
-		onPress:function(oEvent){
+		onPress:function(oEvent){ 
+			var oItem = oEvent.getSource();
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("detail");
+			oRouter.navTo("detail",{
+				invoicePath:oItem.getBindingContext("invoice").getPath().substr(1)
+			});
 		}
  
 	});
